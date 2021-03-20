eb3246146ee260b9fd5dfff7f3560d512c4af713
Leejinwan <leejinwan99@gmail.com>
2021-03-21 00:24:39 +0900

    for2

--- for1.py
+++ for1.py
@@ -1,2 +1,12 @@
-for number in range(10):
-    print(number)
+names = ['一色','かわさき','かわさきしんじ','遠藤']
+target = 'かわさき'
+found = False
+
+for name in names:
+    if target in name:
+        print(f'発見:{name}')
+        found = True
+        continue
+    print('繰り返し処理を継続します。')
+if not found:
+    print('見つかりませんでした。')
